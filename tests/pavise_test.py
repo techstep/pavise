18b96df40544c9187a281430fb76d8795cbc7452
Rob Jefferson <techstep@gmail.com>
2021-03-02 10:55:03 -0500

    Adding a test for retrieving added URL

--- tests/pavise_test.py
+++ tests/pavise_test.py
@@ -58,3 +58,13 @@ def test_can_insert_url():
 
     assert response.content_type == "application/json"
     assert response.json["hash"] == "b27b1508de40c33cc18aeaa945632dc3ffbc72c13e81cd214b154859abbc0c64"
+
+
+def test_can_retrieve_inserted_url():
+    url = "http://127.0.0.1:5000/urlinfo/1/badsite.org"
+    tester = app.test_client()
+    response = tester.get(url)
+
+    assert response.status_code == 200
+    result = json.loads(response.data)
+    assert result["is_malware"] == True
